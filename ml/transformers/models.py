7f17f89bcbd8b1b8818a643eddc8ba2abe747e98
oikonomod <oikonomod@gmail.com>
2024-09-25 22:51:48 +0300

    Multihead attention

--- ml/transformers/models.py
+++ ml/transformers/models.py
@@ -5,32 +5,49 @@ import torch.nn as nn
 
 
 class AttentionBlock(nn.Module):
-
-    def __init__(self, d: int, L: int, input_dim: int, output_dim: int):
+    """
+    Takes an input with d_model dimension. Creates Q, K, V and produces an output with d_k dimension
+    """
+    def __init__(self, d_k: int, d_model: int):
 
         super().__init__()
 
-        self.d = d
-        self.W_Q = nn.Linear(input_dim, d)
-        self.W_K = nn.Linear(input_dim, d)
-        self.W_V = nn.Linear(input_dim, d)
-        self.W_O = nn.Linear(d, output_dim)
+        self.d_k = d_k
+        self.W_Q = nn.Linear(d_model, d_k)
+        self.W_K = nn.Linear(d_model, d_k)
+        self.W_V = nn.Linear(d_model, d_k)
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
 
-        # x has shape (N, L, input_dim)
+        # x has shape (N, L, d_model)
 
-        q = self.W_Q(x)  # shape of (N, L, d)
-        k = self.W_K(x)  # shape of (N, L, d)
-        v = self.W_V(x)  # shape of (N, L, d)
+        q = self.W_Q(x)  # shape of (N, L, d_k)
+        k = self.W_K(x)  # shape of (N, L, d_k)
+        v = self.W_V(x)  # shape of (N, L, d_k)
 
-        attention = self.softmax(torch.bmm(q, k.transpose(1, 2)) / math.sqrt(self.d))  # shape of (N, L, L)
+        attention = self.softmax(torch.bmm(q, k.transpose(1, 2)) / math.sqrt(self.d_k))  # shape of (N, L, L)
 
-        values_with_attention = torch.bmm(attention, v)  # shape of (N, L, d)
-        output = self.W_O(values_with_attention)  # shape of (N, L, output_dim)
+        output = torch.bmm(attention, v)  # shape of (N, L, d_k)
 
         return output, attention
 
 
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, n_heads: int, d_k: int, d_model: int):
+        super().__init__()
+        self.attention_heads = nn.ModuleList(
+            [AttentionBlock(d_k, d_model) for _ in range(n_heads)]
+        )
+        self.W_concat = nn.Linear(d_k * n_heads, d_model)
+
+    def forward(self, x):
+
+        # x has shape (N, L, d_model)
+        attention_heads = torch.cat(
+            [attention_head(x)[0] for attention_head in self.attention_heads], dim=-1
+        )
+        output = self.W_concat(attention_heads)
+        return output
 
--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from ml.transformers.models import AttentionBlock
+from ml.transformers.models import AttentionBlock, MultiHeadAttention
 
 
 class TestTransformers:
@@ -10,18 +10,32 @@ class TestTransformers:
     def test_attention_block(self):
 
         N = 100
-        d = 64
+        d_k = 64
         L = 256
-        input_dim = 32
-        output_dim = 256
+        d_model = 256
 
-        attention_block = AttentionBlock(d=d, L=L, input_dim=input_dim, output_dim=128)
+        attention_block = AttentionBlock(d_k=d_k, d_model=d_model)
 
-        X = torch.randn((N, L, input_dim))
+        X = torch.randn((N, L, d_model))
 
         output, attention = attention_block(X)
 
-        assert output.shape == (N, L, output_dim)
+        assert output.shape == (N, L, d_k)
 
         # each attention column sums to 1. Applied the rounding due to floating errors (output is 0.99999 etc)
         assert (attention.detach().sum(axis=1).apply_(lambda x: round(x, 1)) == torch.ones((N, L))[0][0]).all()
+
+    def test_multi_head_attention(self):
+
+        N = 100
+        d_k = 64
+        L = 111
+        n_heads = 8
+        d_model = 512
+
+        multi_head_attention = MultiHeadAttention(n_heads, d_k, d_model)
+
+        X = torch.randn((N, L, d_model))
+        output = multi_head_attention(X)
+
+        assert output.shape == (N, L, d_model)
